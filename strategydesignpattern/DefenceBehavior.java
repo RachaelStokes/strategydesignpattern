936530c7574c5375bd052a6485e75224f931b4f7
Rachael Stokes <rachaelstokes@Rachaels-MacBook-Pro-2.local>
2021-01-15 15:53:01 -0500

    added comments to block behavior and defence behavior

--- strategydesignpattern/DefenceBehavior.java
+++ strategydesignpattern/DefenceBehavior.java
@@ -1,5 +1,12 @@
 package strategydesignpattern;
-
+/**
+ * interface created that requires all classes that implement it create a play method
+ * @author Rachae
+ */
 public interface DefenceBehavior {
+    /**
+     * method that will be used in the classes that implement this interface that print what type of play they are making
+     * @return a string that explains the play that the player is making
+     */
     public String play(); 
 }
