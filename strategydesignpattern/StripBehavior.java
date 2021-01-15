9f1317143d7b5e13dbba0ba21ea22bd49c84d24e
Rachael Stokes <rachaelstokes@Rachaels-MacBook-Pro-2.local>
2021-01-15 16:51:25 -0500

    added comments to the remaining classes

--- strategydesignpattern/StripBehavior.java
+++ strategydesignpattern/StripBehavior.java
@@ -1,6 +1,15 @@
 package strategydesignpattern;
+/**
+ * a class that implements that defence behavior interface that specifies a strip behavior for a player
+ * @author Rachael
+ */
 
 public class StripBehavior implements DefenceBehavior{
+
+    /**
+     * method to print what the play is
+     * @return a string stating that the play is to strip a ball from runners hands
+     */
     public String play(){
         return "Strip a ball from runners hands";
     }
